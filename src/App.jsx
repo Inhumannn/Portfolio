5a968f17af36ee819fab6aa009c7cebab321a74e
Inhumannn <thomas.pnbm@gmail.com>
2025-03-21 16:15:31 +0100

    refactor: streamline page rendering and enhance header animations

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,36 @@ import Header from "./components/header";
 import Contact from "./pages/contact";
 import Experience from "./pages/experience";
 import Home from "./pages/home";
-import NotFound from "./pages/notfound";
 import Portfolio from "./pages/portfolio";
 import Temoignages from "./pages/temoignages";
 
-function App(){
-  const [currentPage, setCurrentPage] = useState("home")
-  const renderPage = () => {
-    switch(currentPage){
-      case "home":
-        return <Home />
-      case "experience":
-        return <Experience />
-      case "portfolio":
-        return <Portfolio />
-      case "temoignages":
-        return <Temoignages />
-      case "contact":
-        return <Contact />
-      default:
-        return <NotFound />
-    }
-  }
+function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+  const pages = {
+    home: <Home />,
+    experience: <Experience />,
+    portfolio: <Portfolio />,
+    temoignages: <Temoignages />,
+    contact: <Contact />,
+  };
+  const renderPage = pages[currentPage];
   const transition = {
     duration: 0.8,
     delay: 0.5,
-    ease: [0, 0.71, 0.2, 1.01]
-  }
+    ease: [0, 0.71, 0.2, 1.01],
+  };
   return (
     <>
       <div className="relative border border-[#363636] bg-[#1e1e1f] rounded-[15px] pt-0 px-[35px] pb-[35px]">
         <div className="bg-[#1e1e1f] rounded-[15px]">
-          <Header title={currentPage === "home" ? "À propos de moi" : currentPage === "experience" ? "Expérience" :  currentPage === "portfolio" ? "Portfolio" : currentPage === "temoignages" ? "Témoignages" : currentPage === "contact" ? "Contact" : "Page non trouvée"} setCurrentPage={setCurrentPage}/>
-          <hr className="w-[77px] h-[2px] bg-[#a6a6a6] border-none rounded-[5px] mt-[-9px] mr-0 mb-[22px] ml-0" />
-          <motion.div key={currentPage} initial={{ opacity: 0, z: 100 }} animate={{ opacity: 1, z: 0 }}  transition={transition}>
-          {renderPage()}
-          </motion.div>
+          <Header title={ currentPage === "home" ? "À propos de moi" : currentPage === "experience" ? "Expérience" : currentPage === "portfolio" ? "Portfolio" : currentPage === "temoignages" ? "Témoignages" : currentPage === "contact" ? "Contact" : "Page non trouvée"
+            } setCurrentPage={setCurrentPage}/>
+          
+          <motion.div key={currentPage} initial={{ opacity: 0, z: 100 }} animate={{ opacity: 1, z: 0 }} exit={{ opacity: 0, z: -100 }} transition={transition}>{renderPage}</motion.div>
         </div>
       </div>
-    <Footer />
-  </>
+      <Footer />
+    </>
   )
 }
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,35 @@
-function Header({ setCurrentPage, title }){
-  return(
+import { motion } from "framer-motion";
+
+function Header({ setCurrentPage, title }) {
+  const transition = {
+    duration: 0.8,
+    delay: 0.5,
+    ease: [0, 0.71, 0.2, 1.01],
+  };
+  return (
     <header className="flex justify-between">
-      <h1 className="pt-[26px] text-[30px] font-semibold text-[#d6d6d6]">{title}</h1>
+      <motion.h1
+        className="pt-[26px] text-[30px] font-semibold text-[#d6d6d6]"
+        key={title}
+        initial={{ opacity: 0, z: 100 }}
+        animate={{ opacity: 1, z: 0 }}
+        exit={{ opacity: 0, z: -100 }}
+        transition={transition}
+      >
+        {title}
+      <motion.div className="w-[77px] h-[2px] bg-[#a6a6a6] border-none rounded-[5px] mt-[-9px] mr-0 mb-[22px] ml-0" initial={{ opacity: 0, z: 100 }} animate={{ opacity: 1, z: 0 }} exit={{ opacity: 0, z: -100 }} transition={transition}></motion.div>
+      </motion.h1>
       <nav className="absolute right-[0] top-[0] content-center bg-[#282829] border border-[#363636] rounded-tr-[12px] rounded-bl-[12px] py-[20px] px-[30px] ">
         <ul className="flex gap-[30px] list-none">
-        <li><button onClick={() => setCurrentPage('home')} className="text-[14px] text-[#a2a2a2]">À propos</button></li>
-        <li><button onClick={() => setCurrentPage('experience')} className="text-[14px] text-[#a2a2a2]">Experience</button></li>
-        <li><button onClick={() => setCurrentPage('portfolio')} className="text-[14px] text-[#a2a2a2]">Portfolio</button></li>
-        <li><button onClick={() => setCurrentPage('temoignages')} className="text-[14px] text-[#a2a2a2]">Temoignages</button></li>
-        <li><button onClick={() => setCurrentPage('contact')} className="text-[14px] text-[#a2a2a2]">Contact</button></li>
+          <li><button onClick={() => setCurrentPage("home")} className="text-[14px] text-[#a2a2a2]">À propos</button></li>
+          <li><button onClick={() => setCurrentPage("experience")} className="text-[14px] text-[#a2a2a2]">Experience</button></li>
+          <li><button onClick={() => setCurrentPage("portfolio")} className="text-[14px] text-[#a2a2a2]">Portfolio</button></li>
+          <li><button onClick={() => setCurrentPage("temoignages")} className="text-[14px] text-[#a2a2a2]">Témoignages</button></li>
+          <li><button onClick={() => setCurrentPage("contact")} className="text-[14px] text-[#a2a2a2]">Contact</button></li>
         </ul>
       </nav>
     </header>
   )
 }
-export default Header
+
+export default Header;
